f938235aa8a026df2254cbe321d579f809c41983
patxu <patrickxu13@gmail.com>
2017-05-10 12:13:41 -0400

    linting

--- sa3/src/index.js
+++ sa3/src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+
+let timer = 0;
+
+$('#main').html(`You've been on this page for ${timer} seconds.`);
+
+setInterval(() => {
+  timer += 1;
+  $('#main').html(`You've been on this page for ${timer} seconds.`);
+}, 1000);
